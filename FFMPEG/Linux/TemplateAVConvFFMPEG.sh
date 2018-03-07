1daf9afd06a08c35c3dd3d463eee7155c6707951
Eduardo Macedo Santos <dudmacedo@gmail.com>
2018-03-06 21:06:15 -0300

    Primeira versão concluída para Template FFMPEG para converter Vídeos

--- FFMPEG/Linux/TemplateAVConvFFMPEG.sh
+++ FFMPEG/Linux/TemplateAVConvFFMPEG.sh
@@ -8,35 +8,44 @@
 # 
 # * codec H264 - $H264
 # * codec H265 - $H265
+# * tamanho do vídeo   960x540 -  $R540P
+# * tamanho do vídeo  1280x720 -  $R720P
+# * tamanho do vídeo 1920x1080 - $R1080P
 # 
 # Para áudio:
 # 
-# codec AC3 para todos os áudios - $AC3A
-# codec AC3 para o stream 0 - $AC30
-# codec AC3 para o stream 1 - $AC31
-# codec MP3 para todos os áudios - $MP3A
-# codec MP3 para o stream 0 - $MP30
-# codec MP3 para o stream 1 - $MP31
-# bitrate 128kbit para todos os streams
-# bitrate 192kbit para todos os streams
-# bitrate 256kbit para todos os streams
-# bitrate 384kbit para todos os streams
-# bitrate 640kbit para todos os streams
-# bitrate 128kbit para o stream 0
-# bitrate 128kbit para o stream 1
-# bitrate 192kbit para o stream 0
-# bitrate 192kbit para o stream 1
-# bitrate 256kbit para o stream 0
-# bitrate 256kbit para o stream 1
-# bitrate 384kbit para o stream 0
-# bitrate 384kbit para o stream 1
-# bitrate 640kbit para o stream 0
-# bitrate 640kbit para o stream 1
+# * codec AC3 para todos os áudios - $AC3A
+# * codec AC3 para o stream 0 - $AC30
+# * codec AC3 para o stream 1 - $AC31
+# * codec MP3 para todos os áudios - $MP3A
+# * codec MP3 para o stream 0 - $MP30
+# * codec MP3 para o stream 1 - $MP31
+# * bitrate 128kbit para todos os streams - $B128KA
+# * bitrate 192kbit para todos os streams - $B192KA
+# * bitrate 256kbit para todos os streams - $B256KA
+# * bitrate 384kbit para todos os streams - $B384KA
+# * bitrate 640kbit para todos os streams - $B640KA
+# * bitrate 128kbit para o stream 0 - $B128K0
+# * bitrate 128kbit para o stream 1 - $B128K1
+# * bitrate 192kbit para o stream 0 - $B192K0
+# * bitrate 192kbit para o stream 1 - $B192K1
+# * bitrate 256kbit para o stream 0 - $B256K0
+# * bitrate 256kbit para o stream 1 - $B256K1
+# * bitrate 384kbit para o stream 0 - $B384K0
+# * bitrate 384kbit para o stream 1 - $B384K1
+# * bitrate 640kbit para o stream 0 - $B640K0
+# * bitrate 640kbit para o stream 1 - $B640K1
 
 # Vídeo H264
 H264="-c:v libx264"
 # Vídeo H265
 H265="-c:v libx265 -x265-params crf=24:qcomp=0.8:aq-mode=1:aq_strength=1.0:qg-size=16:psy-rd=0.7:psy-rdoq=5.0:rdoq-level=1:merange=44"
+# Tamanho do vídeo   960x540
+R540P="-s 960x540"
+# Tamanho do vídeo  1280x720
+R720P="-s 1280x720"
+# Tamanho do vídeo 1920x1080
+R1080P="-s 1920x1080"
 
 # Áudio AC3 para todos os streams de áudio
 AC3A="-c:a ac3"
@@ -84,19 +93,6 @@ B256K1="-b:a:1 256k"
 B384K1="-b:a:1 384k"
 # Bitrate 640kbps para o stream de áudio 1
 B640K1="-b:a:1 640k"
-
-# bitrate 128kbit para todos os streams
-# bitrate 192kbit para todos os streams
-# bitrate 384kbit para todos os streams
-# bitrate 640kbit para todos os streams
-# bitrate 128kbit para o stream 0
-# bitrate 128kbit para o stream 1
-# bitrate 192kbit para o stream 0
-# bitrate 192kbit para o stream 1
-# bitrate 384kbit para o stream 0
-# bitrate 384kbit para o stream 1
-# bitrate 640kbit para o stream 0
-# bitrate 640kbit para o stream 1
 
 
 
